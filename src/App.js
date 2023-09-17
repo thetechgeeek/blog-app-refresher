5a6e66191566a3c3cc0c2029b8b0dfab571d067c
Rupesh, Rupesh <rrupesh@cvent.com>
2023-09-17 18:40:40 +0530

    Exported data from corrected data.json, designed blog-cards and blog-container in App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { data } from './data';
 
 function App() {
     return (
-        <div>
-            <h1>Hello</h1>
-            <button className='btn btn-primary'>Test button</button>
-        </div>
+        <>
+            <nav class='navbar navbar-expand-lg bg-body-tertiary'>
+                <div class='container-fluid justify-content-end p-1 mx-3'>
+                    <ul class='navbar-nav '>
+                        <li class='nav-item'>
+                            <a class='btn btn-primary' href='#'>
+                                Create Post
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className='container fluid blog-container'>
+                {data.map((item) => (
+                    <div className='blog-card'>{item.title}</div>
+                ))}
+            </div>
+        </>
     );
 }
 
